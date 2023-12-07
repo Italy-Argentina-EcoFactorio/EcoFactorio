daf42c54bde63227126ec74d75dacb9abd7c5992
Aleksander Sardara <86119224+galactic1106@users.noreply.github.com>
2023-12-08 00:17:29 +0100

    Changed UI render mode+ room selection bug
    Changed UI render mode:
    UI now renders on the camera
    
    Room selection bug:
    since the room selection used prefabs to display the room they also had the resource generation script and started producing resources, i removed the script from the models in the buttons to fix the bug

--- Ecofactorio/Assets/Scripts/MouseController.cs
+++ Ecofactorio/Assets/Scripts/MouseController.cs
@@ -52,11 +52,6 @@ public class MouseController : MonoBehaviour
             {
                 callRoomSelectionMenu();
             }
-               
-             
-            
-        
-
         }
     }
 
